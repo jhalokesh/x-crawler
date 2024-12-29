459f3e00730739d9d15fad3149795101dbda9be8
jhalokesh <lokeshkumarjha07+github@gmail.com>
2024-12-30 03:15:13 +0530

    feat: Set up Express.js server with basic configuration

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,18 @@
-console.log('X-crawler');
+import app from './app';
+import { Config } from './config';
+
+const startServer = () => {
+    const PORT = Config.PORT;
+    try {
+        app.listen(PORT, () => console.log('Server running', { port: PORT }));
+    } catch (err) {
+        if (err instanceof Error) {
+            setTimeout(() => {
+                process.exit(1);
+            }, 1000);
+        }
+        process.exit(1);
+    }
+};
+
+startServer();
