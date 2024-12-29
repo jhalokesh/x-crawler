2cb70a7033f2c310c4a03ecc8c32b43d31003a67
jhalokesh <lokeshkumarjha07+github@gmail.com>
2024-12-30 03:39:05 +0530

    feat: Add global error handler using HttpError

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,34 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import { HttpError } from 'http-errors';
+import { Config } from './config';
 
 const app = express();
 
 app.get('/', (req, res) => {
     res.send('Web crawler is running...');
+});
+
+// global error handler
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+app.use((err: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const NODE_ENV = Config.NODE_ENV;
+    // TODO - implement logger
+    console.log(err.message);
+    const statusCode = err.statusCode || err.status || 500;
+
+    res.status(statusCode).json({
+        errors: [
+            {
+                type: err.name,
+                msg: err.message,
+                path: '',
+                location:
+                    NODE_ENV === 'development' || NODE_ENV === 'test'
+                        ? err.stack
+                        : '',
+            },
+        ],
+    });
 });
 
 export default app;
