26408c732c5309d74bed9ce071e0ca79f7079657
jhalokesh <lokeshkumarjha07+github@gmail.com>
2025-01-09 19:45:34 +0530

    refactor: Move worker event listeners to queue configuration
    - Relocate worker event listeners from server.ts to config/queue.ts

--- src/config/queue.ts
+++ src/config/queue.ts
@@ -1,4 +1,4 @@
-import { Queue, Worker } from 'bullmq';
+import { Job, Queue, Worker } from 'bullmq';
 import Redis from 'ioredis';
 import { REDIS_CONFIG, validDomainQueueName } from './index';
 import { domainCrawlJob } from '../workers/domainCrawlWorker';
@@ -11,3 +11,12 @@ const connection = new Redis({
 export const validDomainQueue = new Queue(validDomainQueueName, { connection });
 
 export const validDomainWorker = new Worker(validDomainQueueName, domainCrawlJob, { connection });
+
+// TOOD: implement logger
+validDomainWorker.on('completed', (job: Job) => {
+    console.log(`Job completed: ${job.id}`);
+});
+
+validDomainWorker.on('failed', (job: Job | undefined, err) => {
+    console.log(`Job failed: ${err}`);
+});
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
-import { Job } from 'bullmq';
 import app from './app';
 import { Config } from './config';
-import { validDomainWorker } from './config/queue';
 import connectDB from './db';
 
 const startServer = () => {
@@ -22,15 +20,6 @@ const connectDBAndStartServer = async () => {
     try {
         await connectDB();
         startServer();
-        // TOOD: implement logger
-        console.log('Starting worker...');
-        validDomainWorker.on('completed', (job: Job) => {
-            console.log(`Job completed: ${job.id}`);
-        });
-
-        validDomainWorker.on('failed', (job: Job | undefined, err) => {
-            console.log(`Job failed: ${err}`);
-        });
     } catch (error) {
         console.log(`MongoDB connection error :: ${error}`);
     }
